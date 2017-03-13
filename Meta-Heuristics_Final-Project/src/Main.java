7d34833bc5d0d541534cadcb50115b08792c4ef2
IngvarBjarki <ingvarbjarki@gmail.com>
2017-03-13 14:26:03 +0100

    First change..

--- Meta-Heuristics_Final-Project/src/Main.java
+++ Meta-Heuristics_Final-Project/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		System.out.println("The final prject!");
 		for(int project = 0; project< 6; project = project+2){
 			System.out.println(project);
+			System.out.println("wowa");
 		}
 	
 	}
