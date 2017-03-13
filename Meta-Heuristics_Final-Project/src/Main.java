ff422716ae7273b7fd68faddbcd6d41b2cd3b1e5
IngvarBjarki <ingvarbjarki@gmail.com>
2017-03-13 14:13:41 +0100

    inital check in

---
+++ Meta-Heuristics_Final-Project/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	
+	public static void main(String[] args){
+		System.out.println("The final prject!");
+		for(int project = 0; project< 6; project = project+2){
+			System.out.println(project);
+		}
+	
+	}
+
+}
