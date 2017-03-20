b8154e1a48047c6722b0157eb84fe19ef214c421
IngvarBjarki <ingvarbjarki@gmail.com>
2017-03-20 19:14:03 +0100

    allt klappad og klart, nema vantar kannski ad returna einhverju.. en listarnir virdast rettir

--- Meta-Heuristics_Final-Project/src/DataObject.java
+++ Meta-Heuristics_Final-Project/src/DataObject.java
@@ -42,57 +42,46 @@ public class DataObject {
 		int counter = 0;
 		int numSets = 10000;
 		int setSize = 0;
-		int setCounter= -1; // so we start at 0 when adding to the list
-		boolean firstSet = false;
+		int setCounter= 0; 
 		ArrayList<SetObject> sets = new ArrayList<SetObject>(); 
 		List<Integer> singleSet = new ArrayList<Integer>();
+		int temp = 0;
 		while(done == false){
 			
 			this.numCount = nextNumber();
 			if(numCount == -1){
-				// some printing to look at, at the end,  then, ofc, delate
-				System.out.println(sets.size());
-				System.out.println(sets.get(0).getSet());
-				System.out.println(sets.get(1).getSet());
-				System.out.println(sets.get(2).getSet());
-				System.out.println(sets.get(numSets-2).getName());
-				System.out.println(sets.get(numSets-2).getCost());
-				System.out.println(setCounter);
+				// assigning the last "subset"
+				sets.get(setCounter).setSet(new ArrayList<Integer>(singleSet)); 
 				done = true;
 			}
 
-			if(counter < numSets-1){
+			if(counter < numSets){
 				// we were selecting one to many items, we add -1 since we start at 0
 				String setName = "set" + counter;
 				sets.add(new SetObject(setName, numCount));
-				firstSet = true;
+
+				// so we get the right value (how long our first subset is) from the text file
+				// this overwrites so we end upp with the length of the first subset
+				setSize = numCount;
 			}
 			else{
-				if(firstSet){
-					// lets find all the elements for the first set and 
-					// and go to the begining of the loop to start adding to it
-					setSize = numCount;
-					firstSet = false;
-					setCounter++;
-					continue;
-					
-				}
 				
 				if(setSize != singleSet.size()){
-					//while the does not have all the numbers it should have, lets add to it
+					//if the singleSet does not contain all the numbers it should have in its set, lets add them to it
 					singleSet.add(numCount);
 				}
 				else{
 					// lets make the set objects have the "subsets" and clear the current set so we can add to the next one
 					sets.get(setCounter).setSet(new ArrayList<Integer>(singleSet));
 					singleSet.clear();
+					
 					// find out how many elements are in the next set
 					setSize = numCount;
-					setCounter++;
+					setCounter++; // for selecting the next obj in the sets list
 				}
 						
 			}
-			counter ++;
+			counter ++; // counts every number in the text file
 		}
 		// posibly return the objects so we can use them in other objects..
 	}
